5dacac2fd249efad48018bf8620523b43fac084f
Thanik30 <jeng.thanik@gmail.com>
2024-12-13 16:38:28 +0700

    Lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,11 +1,16 @@
+
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
 int main(){	
+	double loan,rate,pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,61 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	double in = loan*rate/100;
+	double total = loan+(loan*rate/100),NB = total - pay;
+	
+    
+    for(int i = 1; NB > 0 ; i++ ){
+        cout << fixed << setprecision(2); 
+        cout << setw(13) << left << i; 
+        cout << setw(13) << left << loan;
+        cout << setw(13) << left << in;
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << pay;
+        cout << setw(13) << left << NB;
+        cout << "\n";
+        loan = NB;
+        in = NB*rate/100;
+        total = NB+(NB*rate/100);
+        NB = total - pay;
+        if(total < pay){
+        	  
+        	in = loan*rate/100;
+        	total = loan+(loan*rate/100);
+        	pay = total;
+        	NB = 0;
+        	cout << fixed << setprecision(2); 
+            cout << setw(13) << left << i+1; 
+            cout << setw(13) << left << loan;
+            cout << setw(13) << left << in;
+            cout << setw(13) << left << total;
+            cout << setw(13) << left << pay;
+            cout << setw(13) << left << NB;
+            cout << "\n";
+            loan =NB;
+        	    
+        	    
+        }
+        
+        	
+    }
+    if(NB < 0){
+        NB =0;
+        pay = total;
+        cout << fixed << setprecision(2); 
+        cout << setw(13) << left << 1; 
+        cout << setw(13) << left << loan;
+        cout << setw(13) << left << in;
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << pay;
+        cout << setw(13) << left << NB;
+        cout << "\n";
+    
+    }
+    
+
+    	
+	
 	
 	return 0;
 }
